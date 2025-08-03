37c0c5841248d721649af73c958817d0af3a6816
Taha Khan <tahakhan@Tahas-MBP.fios-router.home>
2025-08-03 19:09:38 -0400

    testing

--- modules/buckets/buckets.tf
+++ modules/buckets/buckets.tf
@@ -1,3 +1,22 @@
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g., dev, stage, prod)"
+}
+
+variable "gcp_resource_location" {
+  type        = string
+  description = "GCP region or zone"
+}
+
+variable "gcp_project_name" {
+  type        = string
+  description = "GCP project name"
+}
+
+
+
+
+
 resource "google_storage_bucket" "state_bucket" {
   name     = "terraform-tk-terra-${var.environment}"  # becomes "my-app-bucket-dev" or "my-app-bucket-stage"
   environment = var.environment
