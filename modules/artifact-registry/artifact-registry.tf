bd34c5a6552897611e8520d8c8e2f15720afcfa4
Taha Khan <tahakhan@Tahas-MBP.fios-router.home>
2025-08-13 16:27:12 -0400

    added docker artifact

---
+++ modules/artifact-registry/artifact-registry.tf
@@ -0,0 +1,25 @@
+variable "location" {}
+variable "product" {}
+variable "environment" {}
+
+locals {
+    docker_image_registry_name = var.environment == "dev" || var.environment == "stage" ? "testing-repo" : "${var.product}-${var.environment}-${var.location}-docker-repo"
+}
+
+
+resource "google_artifact_registry_repository" "docker_image_registry" {
+    location = var.location
+    repository_id = local.docker_image_registrt_name
+    format = "DOCKER"
+    cleanup_policies {
+        action = "KEEP"
+        id = "${local.docker_image_registry_name}-cleaner"
+        most_recent_versions {
+            keep_count = 2
+            package_name_prefixes = []
+        }
+    }
+    docker_config {
+        immutable_tags = false
+    }
+}
